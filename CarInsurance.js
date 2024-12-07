13b1db22559199358f17f8f0f85528bb297357e4
somesh-SV <125175080+somesh-SV@users.noreply.github.com>
2024-12-07 22:12:47 +0530

    Add files via upload

---
+++ CarInsurance.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const carInsuranceSchema = new mongoose.Schema({
+  insuredName2: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  addressOfInsured: {
+    type: String,
+    required: true,
+  },
+  colorOfCar: {
+    type: String,
+    required: true,
+  },
+  engineNumber: {
+    type: String,
+    required: true,
+  },
+  chasisNumber: {
+    type: String,
+    required: true,
+  },
+  cubicCapacity: {
+    type: String,
+    required: true,
+  },
+  invoiceValueString: {
+    type: Number,
+    required: true,
+  },
+  panNumber: {
+    type: String,
+    required: true,
+  },
+  aadharNo: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("CarInsurance", carInsuranceSchema);
